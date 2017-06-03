cc51334fa8b4376c0f491aeb7c5643c0f01948ea
Dani <martinezcasabuena@gmail.com>
2017-06-03 02:58:30 +0200

    Modificar diseño del item

--- WebEntrega/module/user/view/list_user.php
+++ WebEntrega/module/user/view/list_user.php
@@ -6,13 +6,7 @@
     	<div class="row">
     		<p><a href="index.php?page=controller_user&op=create"><img src="view/img/anadir.png"></a></p>
     		
-    		<table>
-                <tr>
-                    <td width=125><b>Titulo Anuncio</b></th>
-                    <td width=125><b>Direccion </b></th>
-                    <td width=125><b>Precio</b></th>
-                    <th width=350><b>Accion</b></th>
-                </tr>
+    		<table cellpadding = "3">
                 <?php
                     if ($rdo->num_rows === 0){
                         echo '<tr>';
@@ -20,18 +14,19 @@
                         echo '</tr>';
                     }else{
                         foreach ($rdo as $row) {
-                       		echo '<tr>';
-                    	   	echo '<td width=125>'. $row['titulo'] . '</td>';
-                    	   	echo '<td width=125>'. $row['direccion'] . '</td>';
-                    	   	echo '<td width=125>'. $row['precio'] . '</td>';
-                    	   	echo '<td width=350>';
-                    	   	echo '<a class="Button_blue" href="index.php?page=controller_user&op=read&id='.$row['idAnuncio'].'">Read</a>';
-                    	   	echo '&nbsp;';
-                    	   	echo '<a class="Button_green" href="index.php?page=controller_user&op=update&id='.$row['idAnuncio'].'">Update</a>';
-                    	   	echo '&nbsp;';
-                    	   	echo '<a class="Button_red" href="index.php?page=controller_user&op=delete&id='.$row['idAnuncio'].'">Delete</a>';
-                    	   	echo '</td>';
-                    	   	echo '</tr>';
+                            echo '<div>';
+                            echo '<tr bgcolor="blue">';
+                            echo '<td colspan="3"><img src="view/img/not-found.png" alt="" style="width:auto; height:auto;"></td>';
+                            echo '</tr>';
+                            echo '<tr bgcolor="red">';
+                            echo '<td colspan="3" align="center">'. $row['titulo'] . '</td>';
+                            echo '</tr>';
+                            echo '<tr bgcolor="grey">';
+                            echo '<td><a class="Button_blue" href="index.php?page=controller_user&op=read&id='.$row['idAnuncio'].'">Read</a></td>';
+                            echo '<td><a class="Button_green" href="index.php?page=controller_user&op=update&id='.$row['idAnuncio'].'">Update</a></td>';
+                            echo '<td><a class="Button_red" href="index.php?page=controller_user&op=delete&id='.$row['idAnuncio'].'">Delete</a></td>';
+                            echo '</tr>';
+                            echo '</div>';
                         }
                     }
                 ?>
